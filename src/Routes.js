17496c93475a89316da00000724a47d7b10c2df4
Divyanshu Maithani <div.blackcat@gmail.com>
2017-09-14 17:10:04 +0530

    :sparkles: Add Routes and handle authentication

---
+++ src/Routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
+import Home from './Home';
+import Callback from './Callback';
+import Auth from './auth';
+import history from './history';
+
+const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+const Routes = () => (
+  <Router history={history} component={Home}>
+    <div>
+      <Route exact path="/" render={(props) => <Home auth={auth} {...props} />} />
+      <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
+      <Route path="/callback" render={(props) => {
+        handleAuthentication(props);
+        return <Callback {...props} />
+      }}/>
+    </div>
+  </Router>
+);
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import '@appbaseio/reactivesearch/dist/css/style.min.css';
 import 'materialize-css/dist/css/materialize.min.css';
 
 import './index.css';
-import App from './App';
+import Routes from './Routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
