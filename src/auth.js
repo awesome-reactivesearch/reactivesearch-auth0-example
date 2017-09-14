c02fea8706a6ce205dfadf06aac9fbd652e4eab7
Divyanshu Maithani <div.blackcat@gmail.com>
2017-09-14 17:08:32 +0530

    :lock: Add authentication service

---
+++ src/auth.js
@@ -0,0 +1,60 @@
+import auth0 from 'auth0-js';
+
+import history from './history';
+
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: 'divyanshu.auth0.com',
+    clientID: 'TJyKPI6aRiRwgr6SxlT7ExW10NEHW4Vy',
+    redirectUri: 'http://localhost:3000/callback',
+    audience: 'https://divyanshu.auth0.com/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  login = () => {
+    this.auth0.authorize();
+  }
+
+  // parses the result after authentication from URL hash
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        history.replace('/home');
+      } else if (err) {
+        history.replace('/home');
+        console.log(err);
+      }
+    });
+  }
+
+  // Sets user details in localStorage
+  setSession = (authResult) => {
+    // Set the time that the access token will expire at
+    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  // removes user details from localStorage
+  logout = () => {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  // checks if the user is authenticated
+  isAuthenticated = () => {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+}
